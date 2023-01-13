951394949c629fb3f99010cd9087962faf87c4a8
hsc <haoshichuan@foxmail.com>
2023-01-13 17:32:23 +0800

    fist commit remark

---
+++ src/main/java/cn/happymaya/App.java
@@ -0,0 +1,13 @@
+package cn.happymaya;
+
+/**
+ * Hello world!
+ *
+ * @author super
+ */
+public class App {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/cn/happymaya/AppTest.java
@@ -0,0 +1,9 @@
+package cn.happymaya;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+}
